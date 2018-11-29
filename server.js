13dba658dd86912654476d623d75e63b7274243e
Wagner Parisoto <wagnerparisoto@gmail.com>
2018-11-28 23:37:26 -0200

    template

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-heroku'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular-heroku/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angular-heroku/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
---
+++ src/app/app.route.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
+// import { RestaurantsComponent } from './restaurants/restaurants.component';
+// import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+// import { MenuComponent } from './restaurant-detail/menu/menu.component';
+// import { ReviewsComponent } from './restaurant-details/reviews/reviews.component';
+// import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
+// import { NotFoundComponent } from './not-found/not-found.component';
+// import { LoginComponent } from './security/login/login.component';
+// import { LoggedinGuard } from './security/loggedin.guard';
+
+export const ROUTES: Routes = [
+    {path: '', component: HomeComponent}
+    // {path: 'login/:to', component : LoginComponent},
+    // {path: 'login', component : LoginComponent},
+    // {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    //     children: [
+    //         {path: '', redirectTo: 'menu', pathMatch: 'full'},
+    //         {path: 'menu', component: MenuComponent},
+    //         {path: 'reviews', component: ReviewsComponent}
+    //     ]
+    // },
+    // {path: 'restaurants', component: RestaurantsComponent},
+    // {path: 'order', loadChildren: './order/order.module#OrderModule', canLoad: [LoggedinGuard], canActivate: [LoggedinGuard]},
+    // {path: 'order-sumary', component: OrderSumaryComponent},
+    // {path: 'about', loadChildren: './about/about.module#AboutModule'},
+    // {path: '**', component: NotFoundComponent}
+]
